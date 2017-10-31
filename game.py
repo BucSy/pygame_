e6dbbb392bb5470a2081be228abb8097a22a36df
BucSy <bucsyka@citromail.hu>
2017-10-31 17:00:52 +0100

    Added a line..

--- game.py
+++ game.py
@@ -3,20 +3,23 @@ import pygame
 y = 0
 dir = 1
 running = 1
-width = 800
-height = 600
-screen = pygame.display.set_mode((width, height))
-linecolor = 255, 0, 0
-bgcolor = 0, 0, 0
+barheight = 124
+screen = pygame.display.set_mode((800, 600))
+
+barcolor = []
+for i in range(1, 63):
+    barcolor.append((0,0, i*4))
+for i in range(1, 63):
+    barcolor.append((0,0, 255 - i*4))
 
 while running:
     event = pygame.event.poll()
     if (event.type == pygame.QUIT):
         running = 0
-    screen.fill(bgcolor)
-    pygame.draw.line(screen, linecolor, (0, y), (width - 1, y))
-
+    screen.fill((0,0,0))
+    for i in range(0, barheight):
+        pygame.draw.line(screen, barcolor[i], (0, y+i), (799, y+i))
     y += dir
-    if (y == 0 or y == height -1):
+    if (y + barheight > 599 or y < 0):
         dir *= -1
     pygame.display.flip()
