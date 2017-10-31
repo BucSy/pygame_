7c13f1a365bb32ab055b274cd87e64dca62f42be
BucSy <bucsyka@citromail.hu>
2017-10-31 20:30:25 +0100

    Snake is goind on

--- game.py
+++ game.py
@@ -1,48 +1,61 @@
 import pygame
 
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+class MovingPixel:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.hdir = 0
+        self.vdir = -1
+
+    def direction(self, dir):
+        self.hdir, self.vdir = dir
+
+    def move(self):
+        self.x += self.hdir
+        self.y += self.vdir
+
+    def draw(self, surface):
+        surface.set_at((self.x, self.y), (255, 255, 255))
+
 width = 640
 height = 400
-
-x = int(round(width / 2))
-y = int(round(height / 2))
-
-dir_x = 0
-dir_y = -1
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-while running:
-    x += dir_x
-    y += dir_y
+roundedWidth = int(round(width / 2))
+roundedHeight = int(round(height / 2))
 
-    if x <= 0 or x >= width or y <= 0 or y >= height:
+pix = MovingPixel(roundedWidth, roundedHeight)
+
+while running:
+    pix.move()
+
+    if pix.x <= 0 or pix.x >= width or pix.y <= 0 or pix.y >= height:
         print("Crash!")
-        print(x)
-        print(y)
         running = False
 
     screen.fill((0, 0, 0))
-    screen.set_at((x, y), (255, 255, 255))
+    pix.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                dir_x = 0
-                dir_y = -1
-                print("up")
+                pix.direction(UP)
             elif event.key == pygame.K_DOWN:
-                dir_x = 0
-                dir_y = 1
+                pix.direction(DOWN)
             elif event.key == pygame.K_LEFT:
-                dir_x = -1
-                dir_y = 0
+                pix.direction(LEFT)
             elif event.key == pygame.K_RIGHT:
-                dir_x = 1
-                dir_y = 0
+                pix.direction(RIGHT)
 
     pygame.display.flip()
     clock.tick(120)
